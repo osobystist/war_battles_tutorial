44224f4f79015cfeb1961880ffd3b05d8f7df2dc
osobystist <osobystist123gmail.com>
2024-12-28 23:08:35 +0200

    версія закінчена але не працює ресет з першого разу після перемоги

---
+++ main/enemy_rocket.go
@@ -0,0 +1,43 @@
+components {
+  id: "enemy_rocket"
+  component: "/main/enemy_rocket.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"rocket\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/sprites.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    z: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"enemy_rocket\"\n"
+  "mask: \"player\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 12.064516\n"
+  "  data: 7.7142854\n"
+  "  data: 10.0\n"
+  "}\n"
+  ""
+}
---
+++ main/tank.go
@@ -0,0 +1,43 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"tank_down\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/sprites.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    y: -1.0
+  }
+  scale {
+    x: 1.25
+    y: 1.25
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"tanks\"\n"
+  "mask: \"rockets\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 10.0\n"
+  "  data: 12.196796\n"
+  "  data: 10.0\n"
+  "}\n"
+  ""
+}
